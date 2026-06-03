b5c598163bb350f3a77dea87ca646531c7943506
Hanvi-19 <bigullasoundarya@gmail.com>
2026-06-03 17:59:38 +0530

    modified settres

--- src/main/java/com/minikube/sample/properties/PropertiesConfig.java
+++ src/main/java/com/minikube/sample/properties/PropertiesConfig.java
@@ -12,4 +12,21 @@ import org.springframework.stereotype.Component;
 public class PropertiesConfig {
     private String name;
     private String test;
+
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for test
+    public String getTest() {
+        return test;
+    }
+    public void setTest(String test) {
+        this.test = test;
+    }
 }
