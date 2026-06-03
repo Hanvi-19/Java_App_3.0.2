d195fe5e0f1959d1f93ad3b5accdfb662e2b3ca1
Hanvi-19 <bigullasoundarya@gmail.com>
2026-06-03 17:45:04 +0530

    Refactored added

--- src/main/java/com/minikube/sample/rest/controller/HomeResource.java
+++ src/main/java/com/minikube/sample/rest/controller/HomeResource.java
@@ -1,12 +1,9 @@
 package com.minikube.sample.rest.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minikube.sample.properties.PropertiesConfig;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Lookup;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,22 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeResource {
 
-	@Autowired
-	PropertiesConfig config;
+    @Autowired
+    private PropertiesConfig config;
 
     @GetMapping("/data")
     public ResponseEntity<ResponseData> getData() {
         ResponseData responseData = new ResponseData();
-        responseData.setId(1);
-        responseData.setName(config.getName());
-        responseData.setPlace("Hyderabad");
-        responseData.setValue(config.getTest());
+        responseData.setId(1);                       
+        responseData.setName(config.getName());       
+        responseData.setPlace("Hyderabad");           
+        responseData.setValue(config.getTest());      
         return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 
     @Getter
     @Setter
-    public class ResponseData {
+    public static class ResponseData {
         private String name;
         private Integer id;
         private String place;
