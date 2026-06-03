ad3f50db99e51778b5c1d915e092b8f5885013b3
Hanvi-19 <bigullasoundarya@gmail.com>
2026-06-03 17:47:25 +0530

    modified

--- src/main/java/com/minikube/sample/properties/PropertiesConfig.java
+++ src/main/java/com/minikube/sample/properties/PropertiesConfig.java
@@ -2,17 +2,14 @@ package com.minikube.sample.properties;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-
-@Configuration
-@ConfigurationProperties(prefix = "app.data")
 @Getter
 @Setter
+@Component
+@ConfigurationProperties(prefix = "app")
 public class PropertiesConfig {
-
-	private String test;
-	private String name;
+    private String name;
+    private String test;
 }
